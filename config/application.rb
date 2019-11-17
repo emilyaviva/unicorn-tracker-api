84e579ab7f920eb88cf46e000a53199be5b7233a
Emily Kapor-Mater <emily@emilyaviva.com>
2019-11-17 10:10:25 -0800

    Better CORS configuration

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,5 @@ module UnicornApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Allow CORS from localhost
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost:3000',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(',')
-    }
   end
 end
